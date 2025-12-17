478ff8c0f6eeb22ec4ecdd399dbb62add4432e0b
Tisa <tisatandel@gmail.com>
2025-12-17 14:38:00 +0530

    all done

--- mywebsite/src/app/app.routes.ts
+++ mywebsite/src/app/app.routes.ts
@@ -9,12 +9,35 @@ import { Facebook } from './facebook/facebook';
 import { Instragram } from './instragram/instragram';
 import { Twitter } from './twitter/twitter';
 import { GreenLab } from './green-lab/green-lab';
+import { FruitsVegetables } from './categories/fruits-vegetables/fruits-vegetables';
+import { BakeryCakesDairy } from './categories/bakery-cakes-dairy/bakery-cakes-dairy';
+import { DryfruitsOilsMasalas } from './categories/dryfruits-oils-masalas/dryfruits-oils-masalas';
+import { HotBeveragesColdBeverages } from './categories/hot-beverages-cold-beverages/hot-beverages-cold-beverages';
+import { Snacks } from './categories/snacks/snacks';
+import { BeautyHygiene } from './categories/beauty-hygiene/beauty-hygiene';
+import { CleaningHousehold } from './categories/cleaning-household/cleaning-household';
+import { KitchenGardenPets } from './categories/kitchen-garden-pets/kitchen-garden-pets';
+import { EggsMeatFish } from './categories/eggs-meat-fish/eggs-meat-fish';
+import { Babycare } from './categories/babycare/babycare';
 
 export const routes: Routes = [
     { path:'home', component:Home},
     { path:'about', component:About},
     { path:'contact', component:Contact},
-    { path:'categories', component:Categories},
+    { path:'categories', component:Categories,
+      children: [
+      { path: 'fruits-vegetables', component: FruitsVegetables },
+      { path: 'bakery-cakes-dairy', component: BakeryCakesDairy },
+      { path: 'dryfruits-oils-masalas', component: DryfruitsOilsMasalas },
+      { path: 'hot-beverages-cold-beverages', component:HotBeveragesColdBeverages  },
+      { path: 'snacks', component:Snacks  },
+      { path: 'beauty-hygiene', component:BeautyHygiene  },
+      { path: 'cleaning-household', component:CleaningHousehold },
+      { path: 'kitchen-garden-pets', component:KitchenGardenPets  },
+      { path: 'eggs-meat-fish', component:EggsMeatFish  },
+      { path: 'babycare', component: Babycare },
+      ]
+},
     { path:'grocery', component:Grocery},
     { path:'facebook', component:Facebook},
     { path:'instragram', component:Instragram},
